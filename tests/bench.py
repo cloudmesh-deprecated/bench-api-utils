bbf4532233462fc53023a2b28bfd9a980a7bf77b
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2016-06-17 16:01:55 -0400

    add tests for benchmark runner

---
+++ tests/bench.py
@@ -0,0 +1,94 @@
+
+
+from cloudmesh_bench_api.bench import AbstractBenchmarkRunner
+
+from hypothesis import given, settings, assume
+from hypothesis import strategies as st
+
+import os
+import time
+import string
+import random
+
+MAX_SECONDS = 1
+
+
+
+def sleep():
+    f = random.random()
+    i = random.randint(0, MAX_SECONDS)
+    wait = i + f
+    time.sleep(wait)
+
+
+
+class ExampleBenchmarkRunner(AbstractBenchmarkRunner):
+
+    def _fetch(self, prefix):
+        directory = 'dummy'
+        path = os.path.join(prefix, directory)
+        if not os.path.exists(path):
+            os.makedirs(path)
+        return path
+
+    def _prepare(self):
+        sleep()
+        return dict()
+
+    def _launch(self):
+        sleep()
+
+    def _deploy(self):
+        sleep()
+
+    def _run(self):
+        sleep()
+
+    def _verify(self):
+        success = random.choice([True, False])
+        return success
+
+    def _clean(self):
+        sleep()
+
+
+@st.composite
+def filenames(draw):
+    name = draw(st.text(
+        alphabet = string.ascii_letters + string.digits + '_.',
+        min_size = 1,
+    ))
+
+    return name
+
+
+@given(filenames(), st.integers(max_value=5))
+@settings(timeout=120)
+def test_runners(prefix, times):
+
+    assume(times > 0)
+
+    prefix = os.path.join('testprefix', prefix)
+    print 'X', times, 'in', prefix
+    b = ExampleBenchmarkRunner(prefix=prefix)
+    b.bench(times=times)
+    for n in b._timer.names:
+        print n, times, b._timer.average(n)
+    print
+
+    assert b._timer._order == ['fetch', 'prepare', 'launch',
+                        'deploy', 'run', 'cleanup'],  b._timer._order
+
+    assert set(b._timer.keys()) == set(b._order), \
+        (b._timer.keys(), b._timer._order)
+    print 'ok'
+
+
+
+
+if __name__ == '__main__':
+
+    test_runners()
+
+
+
